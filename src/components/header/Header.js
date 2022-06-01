b6550f9d9c991b59946c473cbd68e477f7a57a92
Alexander <ahansen1335@gmail.com>
2022-06-01 16:19:56 +0200

    create basic header with beginning score state

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState, } from 'react'
 
 export default function Header() {
-  return (
-    <div>Header</div>
+  const [score, setScore] = useState(0);
+  
+    return (
+    <div className="header-container">
+        <h1>Memory Game</h1>
+        <div className="score">Score: {setScore(score+1)}</div>
+        <div className="score">Best Score: </div>
+        <p className="instructions">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
   )
 }
