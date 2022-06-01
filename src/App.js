7872c5ab70e9c2a22789a96119332fd8050e10f7
Alexander <ahansen1335@gmail.com>
2022-06-01 15:42:05 +0200

    create basic component structure divs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "./components/header/Header";
+import Game from "./components/game/Game";
 
 function App() {
   return (
-   null
+    <>
+      <Header />
+      <Game />
+    </>
   );
 }
 
